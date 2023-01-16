1b3ca94f78caa31b3ccb7b2f3b947f61dc5c905b
Leynad <matei_daniel@outlook.com>
2023-01-17 00:37:38 +0100

    Extend drag function to succesfully check if the horizontal position is valid for a ship to be placed

--- src/utilityFunctions.js
+++ src/utilityFunctions.js
@@ -5,25 +5,64 @@ const { getDOMElements, myDOMFunctions } = require("./DOM-interaction");
 function utilityFunctions(argument){
 
 
+  function checkPositionValidity(){
+
+  }
+
   function positionPlayerFleet() {
     
     const myDOM = getDOMElements();
     const myDOMFunc = myDOMFunctions();
 
-    console.log('now inside position')
+
     myDOM.boatImgToDrag.addEventListener("dragstart", (e) => {
       e.dataTransfer.setData("text/plain", myDOM.boatImgToDrag.id);
     });
 
     myDOM.firstPlayerDivs.forEach((element) => {
 
+      let elementX = element.getAttribute("div-coordinate-x");
+      let elementY = element.getAttribute("div-coordinate-y");
+
+      const firstCoordinateX = Number(String(elementY) + String(elementX))
+      let secondCoordinate;
+
+      const shipPosition = myDOM.boatImgToDrag.getAttribute("data-ship-position");
+      console.log(shipPosition)
+      const shipLength = myDOM.boatImgToDrag.getAttribute("data-ship-length");
+      console.log(shipLength)
+    
       element.addEventListener("dragover", (e) => {
         e.preventDefault();
+
         element.classList.add("drop-zone-over");
+
+        elementX = element.getAttribute("div-coordinate-x");
+        elementY = element.getAttribute("div-coordinate-y");
+
+      
+        console.log(firstCoordinateX)
+        console.log(secondCoordinate)
+      
+        
+        if(shipPosition ==="horizontal"){
+          secondCoordinate = firstCoordinateX+(shipLength-1)*10
+          // console.log(secondCoordinate)
+        } else if(shipPosition === "vertical"){
+          secondCoordinate = firstCoordinateX + (shipLength-1)
+          // console.log(secondCoordinate)
+        }
+
+        if(secondCoordinate>99){
+          element.classList.remove("drop-zone-over");
+          element.classList.add("drop-zone-over-error");
+        }
+
       });
 
       element.addEventListener("dragleave", (e) => {
         element.classList.remove("drop-zone-over");
+        element.classList.remove("drop-zone-over-error");
       });
 
       element.addEventListener("drop", (e) => {
@@ -32,29 +71,18 @@ function utilityFunctions(argument){
         const droppedElementId = e.dataTransfer.getData("text/plain");
         const droppedElement = document.getElementById(droppedElementId);
 
-        element.appendChild(droppedElement);
-        element.classList.remove("drop-zone-over");
+        if(secondCoordinate>99){
+          e.preventDefault()
+          element.classList.remove("drop-zone-over-error");
+          
+        } else {
+          element.appendChild(droppedElement);
+          element.classList.remove("drop-zone-over");
+          argument.placeShip(shipLength, firstCoordinateX, secondCoordinate, shipPosition, "destroyer");
+          argument.addShipsToBoard();         
+          myDOMFunc.renderShipsOnBoard(argument.shipSquares);
 
-
-        const elementX = element.getAttribute("div-coordinate-x");
-        const elementY = element.getAttribute("div-coordinate-y");
-
-        const firstCoordinateX = Number(String(elementY) + String(elementX))
-        console.log(firstCoordinateX)
-
-
-        argument.placeShip(5, firstCoordinateX, 75, "horizontal", "destroyer");
-        
-        argument.addShipsToBoard();         
-        myDOMFunc.renderShipsOnBoard(argument.shipSquares);
-
-
-        if(argument.ships.length === 1){
-          console.log('length is 1')
-        
         }
-        
-        console.log(argument)
 
       });
     });
